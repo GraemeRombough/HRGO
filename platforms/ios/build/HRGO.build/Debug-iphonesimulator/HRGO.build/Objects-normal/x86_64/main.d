bfcb13a23db2f56b168b7b77714d2c0726245fe3
Graeme Rombough <graemerombough@Graeme-Romboughs-MacBook-Pro.local>
2018-12-10 19:47:55 -0500

    First Upload

---
+++ platforms/ios/build/HRGO.build/Debug-iphonesimulator/HRGO.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/graemerombough/HRGO/platforms/ios/HRGO/HRGO-Prefix.pch \
+  /Users/graemerombough/HRGO/platforms/ios/internal/main.m \
+  /Users/graemerombough/HRGO/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/graemerombough/HRGO/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/graemerombough/HRGO/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/graemerombough/HRGO/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/graemerombough/HRGO/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/graemerombough/HRGO/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/graemerombough/HRGO/platforms/ios/internal/TNSDebugging.h \
+  /Users/graemerombough/HRGO/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
---
+++ platforms/ios/build/emulator/HRGO.app/app/article-page.js
@@ -0,0 +1,25 @@
+var frameModule = require("ui/frame");
+const StackLayout = require("tns-core-modules/ui/layouts/stack-layout").StackLayout;
+const Button = require("tns-core-modules/ui/button/").Button;
+const GridLayout = require("tns-core-modules/ui/layouts/grid-layout").GridLayout;
+const Label = require("tns-core-modules/ui/label/").Label;
+
+
+exports.pageLoaded = function(args) {
+    const page = args.object;
+    // >> stack-layout-code-behind
+    const articleStack = new StackLayout();
+    // Set the orientation property
+    articleStack.orientation = "vertical";
+    articleStack.col = 1;
+    articleStack.className = "Article_MainStack";
+};
+exports.goToLanding = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("landing-page");
+}
+exports.goToHome = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("main-page");
+    
+}
