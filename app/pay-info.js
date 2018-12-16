385f90e0a4d97f8ef08902d1cdb6700bc5f02082
Graeme Rombough <graemerombough@Graeme-Romboughs-MacBook-Pro.local>
2018-12-15 22:23:46 -0500

    got pageData/pay-info working, start returnSalary

--- app/pay-info.js
+++ app/pay-info.js
@@ -10,15 +10,15 @@ exports.onNavigatingTo = function(args){
     const classifications = ["STDNT00", "AS   01", "AS   02", "AS   03", "AS   04"];
     var steps = [1,2,3,4,5,6,7,8];
     const page = args.object;
-    pageVM = page;
-    const vm = fromObject({
+    pageData = fromObject({
         classItems: classifications,
         classIndex: 1,
         stepItems: steps,
         stepIndex:1,
-        infoVisible: true
+        infoVisible: true,
+        salaryVisible: false
     }); 
-    page.bindingContext = vm;
+    page.bindingContext = pageData;
     
 }
 exports.pageLoaded = function(args) {
@@ -49,9 +49,12 @@ exports.onStepListPickerLoaded = function(args){
     });
 }
 exports.getSalaryInfo = function(args){
-    pageData.set("showInfo", true);
+    //pageData.set("showInfo", true);
     //args.object.bindingContext = pageData;
-
+    console.log("getSalaryInfo");
+    pageData.set("annualRate", "100,000");
+    pageData.set("infoVisible", false);
+    pageData.set("salaryVisible", true);
 };
 var loadSteps = function(selectedClass,inputArg){
     console.log('loadSteps');
@@ -69,11 +72,26 @@ var loadSteps = function(selectedClass,inputArg){
         steps.push(i+1);
     }
     console.log(`steps: ${steps.length}`);
+    
 
 };
 exports.goToHome = function(){
     var topmost = frameModule.topmost();
     topmost.navigate("main-page");
-    
+};
+var returnSalary = function(){
+    var databaseReturn = [];
+    var databaseLine = {};
+    databaseLine = {classCode:"STDNT", step:"1", hourly:13.47, daily:101.025, biweekly:1010.25, annually:26266.5};
+    databaseLine = {classCode:"STDNT", step:"2", hourly:14.43, daily:108.225, biweekly:1082.25, annually:28138.5};
+    databaseLine = {classCode:"STDNT", step:"3", hourly:15.42, daily:115.65, biweekly:1156.5, annually:30069};
+    databaseLine = {classCode:"STDNT", step:"4", hourly:16.49, daily:123.675, biweekly:1236.75, annually:32155.5};
+    databaseLine = {classCode:"STDNT", step:"5", hourly:17.64, daily:132.3, biweekly:1323, annually:34398};
+    databaseLine = {classCode:"STDNT", step:"6", hourly:18.91, daily:141.825, biweekly:1418.25, annually:36874.5};
+    databaseLine = {classCode:"STDNT", step:"7", hourly:20.19, daily:151.425, biweekly:1514.25, annually:39370.5};
+    databaseLine = {classCode:"STDNT", step:"1", hourly:21.64, daily:162.3, biweekly:1623, annually:42198};
+    databaseReturn.push(databaseLine);
+
+
 };
 
