73ea76c273f3109732c6f6abbc3f464d2c811622
Graeme Rombough <graemerombough@Graeme-Romboughs-MacBook-Pro.local>
2018-12-17 21:23:00 -0500

    addTeamMember working

--- platforms/ios/HRGO/app/my-team.js
+++ platforms/ios/HRGO/app/my-team.js
@@ -11,6 +11,8 @@ exports.onNavigatingTo = function(args){
     const page = args.object;
     page.bindingContext = pageData;  
     pageObject = page;
+    pageData.set("employeeName", "");
+    pageData.set("employeeEmail", "");
     
 };
 exports.pageLoaded = function(args) {
@@ -24,13 +26,34 @@ exports.addEmployee = function(args){
     var newEmployee = {empName: pageData.employeeName, empEmail: pageData.employeeEmail};
     var employeeString = JSON.stringify(newEmployee);
     console.log(employeeString);
-
-    var layout = pageObject.getViewById("teamListStack");
-    // create dynamic content
-    var label = new Label();
-    label.text = "dynamic";
-    // connect to live view
-    layout.addChild(label);
     
+    var tempTeam = getTeamMembers();
+    tempTeam.push(newEmployee);
+    displayTeamMembers(tempTeam);
+};
+var getTeamMembers = function(){
+    var teamMemberPull;
+    //TO REPLACE
+        var teamMembers = [];
+        teamMembers[0] = {empName: "Person 1", empEmail: "Email 1"};
+        teamMembers[1] = {empName: "Person 2", empEmail: "Email 2"};
+        teamMembers[2] = {empName: "Person 3", empEmail: "Email 3"};
+        teamMemberPull = JSON.stringify(teamMembers);
+    //END REPLACE
+
+    teamMemberReturn = JSON.parse(teamMemberPull);
+
+    return teamMemberReturn;
+};
+var displayTeamMembers = function(membersToDisplay){
+    var teamStack = pageObject.getViewById("teamListStack");
+    var teamMembers = membersToDisplay;
+
+    // create dynamic content
+    for(i = 0; i < teamMembers.length; i++){
+        var teamMemberLabel = new Label;
+        teamMemberLabel.text = teamMembers[i].empName + " ( " + teamMembers[i].empEmail + " )";
+        teamStack.addChild(teamMemberLabel);
+    };
 };
 
