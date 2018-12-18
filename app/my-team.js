0a7636cc0aa37ad319aa2850d0b717f005228aca
Graeme Rombough <graemerombough@Graeme-Romboughs-MacBook-Pro.local>
2018-12-17 21:05:00 -0500

    got dynamic StackLayout add in my-team.js

--- app/my-team.js
+++ app/my-team.js
@@ -2,12 +2,16 @@ var frameModule = require("ui/frame");
 var view = require("ui/core/view");
 var observable = require("data/observable");
 var pageData = new observable.Observable();
+var pageObject;
+const Label = require("tns-core-modules/ui/label/").Label;
 const fromObject = require("tns-core-modules/data/observable").fromObject;
 
 
 exports.onNavigatingTo = function(args){
     const page = args.object;
     page.bindingContext = pageData;  
+    pageObject = page;
+    
 };
 exports.pageLoaded = function(args) {
     
@@ -20,6 +24,13 @@ exports.addEmployee = function(args){
     var newEmployee = {empName: pageData.employeeName, empEmail: pageData.employeeEmail};
     var employeeString = JSON.stringify(newEmployee);
     console.log(employeeString);
+
+    var layout = pageObject.getViewById("teamListStack");
+    // create dynamic content
+    var label = new Label();
+    label.text = "dynamic";
+    // connect to live view
+    layout.addChild(label);
     
 };
 
