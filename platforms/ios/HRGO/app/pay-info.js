79d559aedcde53502d21977e8a82896397feb4fd
Graeme Rombough <graemerombough@Graeme-Romboughs-MacBook-Pro.local>
2019-01-05 21:24:07 -0500

    pay info taking dynamic values

--- platforms/ios/HRGO/app/pay-info.js
+++ platforms/ios/HRGO/app/pay-info.js
@@ -2,14 +2,19 @@ var frameModule = require("ui/frame");
 var view = require("ui/core/view");
 var observable = require("data/observable");
 var pageData = new observable.Observable();
+var selectedClass, selectedStep;
+var pageObject;
 const ListPicker = require("tns-core-modules/ui/list-picker").ListPicker;
 const fromObject = require("tns-core-modules/data/observable").fromObject;
 var pageVM;
 
 exports.onNavigatingTo = function(args){
+    selectedClass = null;
+    selectedStep = null;
     const classifications = ["STDNT00", "AS   01", "AS   02", "AS   03", "AS   04"];
     var steps = [1,2,3,4,5,6,7,8];
     const page = args.object;
+    pageObject = page;
     pageData = fromObject({
         classItems: classifications,
         classIndex: 1,
@@ -35,6 +40,8 @@ exports.onClassListPickerLoaded = function(args){
         vm.set("classIndex", listPicker.selectedIndex);
         console.log(`ListPicker selected value: ${listPicker.selectedValue}`);
         console.log(`ListPicker selected index: ${listPicker.selectedIndex}`);
+        selectedClass = [listPicker.selectedIndex, listPicker.selectedValue];
+        selectedStep = null;
         loadSteps(listPicker.selectedValue,args);
     });
 }
@@ -45,7 +52,10 @@ exports.onStepListPickerLoaded = function(args){
         vm.set("stepIndex", listPicker.selectedIndex);
         console.log(`ListPicker selected value: ${listPicker.selectedValue}`);
         console.log(`ListPicker selected index: ${listPicker.selectedIndex}`);
-        loadSteps(listPicker.selectedValue,args);
+        selectedStep = [listPicker.selectedIndex, listPicker.selectedValue];
+        
+        
+        //loadSteps(listPicker.selectedValue,args);
     });
 }
 exports.getSalaryInfo = function(args){
@@ -54,7 +64,8 @@ exports.getSalaryInfo = function(args){
     console.log("getSalaryInfo");
     
     pageData.set("infoVisible", false);
-    var salaryData = returnSalary("AS   01", "3");
+    if(selectedClass != null && selectedStep !=null){
+    var salaryData = returnSalary(selectedClass[1], selectedStep[1]);
     pageData.set("annualRate", "$" + Math.round(salaryData.annually * 100 + Number.EPSILON ) / 100);
     pageData.set("biweeklyRate", "$" + Math.round(salaryData.biweekly * 100 + Number.EPSILON ) / 100);
     pageData.set("dailyRate", "$" + Math.round(salaryData.daily * 100 + Number.EPSILON ) / 100);
@@ -62,13 +73,13 @@ exports.getSalaryInfo = function(args){
     pageData.set("overtime1Rate", "$" + Math.round((salaryData.hourly * 1.5) * 100 + Number.EPSILON ) / 100);
 
     pageData.set("salaryVisible", true);
-
+    }
 };
 var loadSteps = function(selectedClass,inputArg){
     console.log('loadSteps');
     var numOfSteps;
-    var steps = [];
-    switch(selectedClass){
+    var steps = [1,2,3];
+    /*switch(selectedClass){
         case "STDNT00":
         numOfSteps = 8;
         break;
@@ -78,9 +89,11 @@ var loadSteps = function(selectedClass,inputArg){
     };
     for(i = 0; i < numOfSteps; i++){
         steps.push(i+1);
-    }
+    }*/
     console.log(`steps: ${steps.length}`);
     
+    pageData.stepItems = steps;
+    pageData.stepIndex = 1;
 
 };
 exports.getCalculatedInfo = function(){
