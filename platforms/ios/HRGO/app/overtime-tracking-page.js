3e282fd4f8a91725fa6ddd8810e8191835de3e5d
Graeme Rombough <graemerombough@Graeme-Romboughs-MacBook-Pro.local>
2019-05-27 23:01:12 -0400

    added styling to overtime tables with other cols

--- platforms/ios/HRGO/app/overtime-tracking-page.js
+++ platforms/ios/HRGO/app/overtime-tracking-page.js
@@ -1,6 +1,8 @@
 var frameModule = require("ui/frame");
 var view = require("ui/core/view");
 var dialogs = require("ui/dialogs");
+var buttonModule = require("ui/button");
+const Button = require("tns-core-modules/ui/button/").Button;
 const layout = require("tns-core-modules/ui/layouts/grid-layout");
 var observable = require("data/observable");
 var pageData = new observable.Observable();
@@ -89,31 +91,63 @@ var displayOvertime = function(){
         var gridLayout = new layout.GridLayout();
         var rowTitle = new Label();
         var hoursTitle = new Label();
+        var hoursNumber = new Label();
         var dateInput = new Date(overTimeList[i].Date);
-        var enteredCheck = new switchModule.Switch();
+        var enteredTitle = new Label();
+        var enteredCheck = new Button();
+        var clearButton = new Button();
         
         rowTitle.text = `${dateInput.getFullYear()}\/${dateInput.getMonth()+1}\/${dateInput.getDate()}`;
         rowTitle.className = "Main_Nav_SubLine";
-        hoursTitle.text = "Hours: " + overTimeList[i].Hours;
-        hoursTitle.className = "Article_Body"
-        enteredCheck.checked = overTimeList[i].Entered;
+        hoursTitle.text = "Hours:";
+        hoursTitle.className = "Utility_TableText_1";
+        hoursNumber.text = overTimeList[i].Hours;
+        hoursNumber.className = "Utility_TableText_1";
+        enteredTitle.text = "Entered?";
+        enteredTitle.className = "Utility_TableText_1";
+        enteredCheck.className = "Home_Checkbox";
+        enteredCheck.value = overTimeList[i].Entered;
         enteredCheck.id = i;
-        enteredCheck.on("checkedChange", (args) => {setItemEntered(args);});
+        enteredCheck.on(buttonModule.Button.tapEvent, setItemEntered, this);
+        clearButton.text = "Clear";
+        clearButton.className = "Submit_Button_1";
 
         layout.GridLayout.setRow(rowTitle, 0);
         layout.GridLayout.setRow(hoursTitle, 1);
+        layout.GridLayout.setRow(hoursNumber, 1);
+        layout.GridLayout.setRow(enteredTitle, 2);
         layout.GridLayout.setRow(enteredCheck, 2);
+        layout.GridLayout.setRow(clearButton, 3);
+
+        layout.GridLayout.setColumn(rowTitle, 0);
+        layout.GridLayout.setColumn(hoursTitle, 0);
+        layout.GridLayout.setColumn(hoursNumber, 1);
+        layout.GridLayout.setColumn(enteredTitle, 0);
+        layout.GridLayout.setColumn(enteredCheck, 1);
+        layout.GridLayout.setColumn(clearButton, 0);
         
         gridLayout.addChild(rowTitle);
         gridLayout.addChild(hoursTitle);
+        gridLayout.addChild(enteredTitle);
         gridLayout.addChild(enteredCheck);
+        gridLayout.addChild(hoursNumber);
+        gridLayout.addChild(clearButton);
+
 
         var titleRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
         var hoursRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
         var checkRow= new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+        var clearRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+        var firstCol = new layout.ItemSpec(1, layout.GridUnitType.STAR);
+        var secondCol = new layout.ItemSpec(1, layout.GridUnitType.STAR);
+        layout.GridLayout.setColumnSpan(rowTitle, 2);
+        layout.GridLayout.setColumnSpan(clearButton, 2);
+        gridLayout.addColumn(firstCol);
+        gridLayout.addColumn(secondCol);
         gridLayout.addRow(titleRow);
         gridLayout.addRow(hoursRow);
         gridLayout.addRow(checkRow);
+        gridLayout.addRow(clearRow);
         gridLayout.className = "POC_Grid";
         otLayout.addChild(gridLayout);
     }
@@ -121,7 +155,14 @@ var displayOvertime = function(){
 }
 var setItemEntered = function(args){
     console.log(args.object.id);
-    overTimeList[args.object.id].Entered = args.object.checked;
+    if(args.object.value == "true"){
+        args.object.value = "false";
+        args.object.text = "";
+    }else{
+        args.object.value = "true";
+        args.object.text = "\uea10";
+    }
+    overTimeList[args.object.id].Entered = args.object.value;
     saveOvertime();
 }
 
