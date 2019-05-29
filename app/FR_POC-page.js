d729b74e6a244f08e2431afab43a13cbb69a7c34
Graeme Rombough <graemerombough@Graeme-Romboughs-MacBook-Pro.local>
2019-05-29 15:37:12 -0400

    added french pages

---
+++ app/FR_POC-page.js
@@ -0,0 +1,151 @@
+var frameModule = require("ui/frame");
+var buttonModule = require("ui/button");
+var view = require("ui/core/view");
+var observable = require("data/observable");
+const layout = require("tns-core-modules/ui/layouts/grid-layout");
+const ScrollView = require("tns-core-modules/ui/scroll-view/").ScrollView;
+const Label = require("tns-core-modules/ui/label/").Label;
+const Button = require("tns-core-modules/ui/button/").Button;
+const StackLayout = require("tns-core-modules/ui/layouts/stack-layout").StackLayout;
+var pageData = new observable.Observable();
+const email = require("nativescript-email");
+
+exports.pageLoaded = function(args) {
+   
+    const page = args.object;
+    page.bindingContext = pageData;  
+    pageObject = page;
+    displayPOCs(getFromDatabase());
+};
+
+exports.goToHome = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("main-page");
+    
+};
+exports.goBack = function(args){
+    const thisPage = args.object.page;
+    thisPage.frame.goBack()
+}
+exports.footer3 = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("profile-page");
+    
+}
+exports.footer4 = function(){
+    console.log("Go To Feedback");
+    var topmost = frameModule.topmost();
+    //topmost.navigate("feedback-page");
+    var pageDetails = String(topmost.currentPage).split("///");
+    const TODAY = new Date();
+    var navigationOptions={
+        moduleName:'feedback-page',
+        context:{Language: "ENG",
+                PageName: pageDetails[1].split("/")[1].split(".")[0],
+                DateTime: TODAY
+                }
+            }
+    topmost.navigate(navigationOptions); 
+}
+exports.footer5 = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("POC-page");
+}
+exports.searchPOC = function(){
+    var dataBaseReturn = getFromDatabase();
+    var filteredResults = [];
+    if(pageData.get("SearchCriteria") != ""){
+    for (i = 0; i < dataBaseReturn.length; i++) {
+        if (dataBaseReturn[i].Title.toLowerCase().includes(pageData.get("SearchCriteria").toLowerCase()) == true ){
+            filteredResults.push(dataBaseReturn[i]);
+        }else if(dataBaseReturn[i].Desc.toLowerCase().includes(pageData.get("SearchCriteria").toLowerCase()) == true ){
+            filteredResults.push(dataBaseReturn[i]);
+        }
+    }
+    displayPOCs(filteredResults);
+    }else{
+        displayPOCs(getFromDatabase());
+    }
+    
+}
+var displayPOCs = function(POCs){
+    var POCList = pageObject.getViewById("POC_List");
+    POCList.removeChildren();
+    for(i = 0; i < POCs.length; i++ ){
+        POCList.addChild(createPOCGrid(POCs[i].Title, POCs[i].Phone, POCs[i].Email, POCs[i].Desc));
+    }
+};
+var createPOCGrid = function(POC_t, POC_p, POC_e, POC_d){
+    var gridLayout = new layout.GridLayout();
+    var POCTitle = new Label();
+    var POCPhone = new Button();
+    var POCEmail = new Button();
+    var POCDesc = new Label();
+
+    POCTitle.text = POC_t;
+    POCTitle.className = "POC_H1";
+    POCPhone.text = "Phone: " + POC_p;
+    POCPhone.className = "POC_Phone";
+    POCPhone.phone = POC_p;
+    POCEmail.text = "Email: " + POC_e;
+    POCEmail.className = "POC_Phone";
+    POCEmail.email = POC_e;
+    POCDesc.text = "Description: " + POC_d;
+    POCDesc.className = "POC_Body";
+
+    POCPhone.on(buttonModule.Button.tapEvent, callPOC, this);
+    POCEmail.on(buttonModule.Button.tapEvent, emailPOC, this);
+
+    layout.GridLayout.setRow(POCTitle, 0);
+    layout.GridLayout.setRow(POCPhone, 1);
+    layout.GridLayout.setRow(POCEmail, 2);
+    layout.GridLayout.setRow(POCDesc, 3);
+    gridLayout.addChild(POCTitle);
+    gridLayout.addChild(POCPhone);
+    gridLayout.addChild(POCEmail);
+    gridLayout.addChild(POCDesc);
+    var titleRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+    var phoneRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+    var emailRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+    var descRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+    gridLayout.addRow(titleRow);
+    gridLayout.addRow(phoneRow);
+    gridLayout.addRow(emailRow);
+    gridLayout.addRow(descRow);
+    gridLayout.className = "POC_Grid";
+
+    return gridLayout;
+
+};
+var getFromDatabase = function(){
+    var databaseReturn = [];
+    var dbRow = {};
+
+    dbRow = {Title:"PSPC Call Center", Phone:"1-888-HRTOPAY", Email:"", Desc:"You can contact the Public Service Pay Center (PSPC) as your first stop for any pay related issues."};
+    databaseReturn.push(dbRow);
+    dbRow = {Title:"HRSS Support Line", Phone:"1-833-747-6363", Email:"", Desc:"The Human Resources Services and Support (HRSS) hotline can be contacted for technical issues related to the HRSS system."};
+    databaseReturn.push(dbRow);
+    dbRow = {Title:"Section 34 Manager Support Line", Phone:"1-833-747-6363", Email:"", Desc:"If you are a Section 34 Manager experiencing issues with time approval related tasks, agents at this hotline can help walk through your issues."};
+    databaseReturn.push(dbRow);
+    dbRow = {Title:"PSPC Pension Center", Phone:"1-800-561-7930", Email:"", Desc:"PSPC is responsible for all federal pension plans (Public Service, RCMP and Canadian Armed Forces). If you have questions about your pension, contact the Government of Canada Pension Centre"};
+    databaseReturn.push(dbRow);
+    dbRow = {Title:"Employee Assistance Program", Phone:"1-800-268-7708", Email:"", Desc:"EAP offers solutions to both prevent and address the concerns of employers, employees, and immediate family members."};
+    databaseReturn.push(dbRow);
+    return databaseReturn;
+};
+
+var callPOC = function(eventData){
+    console.log(eventData.object.phone);
+};
+var emailPOC = function(eventData){
+    console.log(eventData.object.email);
+    if (email.available()){
+        email.compose({
+            subject: "",
+            body: "",
+            to: eventData.object.email
+        });
+    } else {
+        console.log("Email Not Available");
+    }
+};
---
+++ app/FR_overtime-tracking-page.js
@@ -0,0 +1,270 @@
+var frameModule = require("ui/frame");
+var view = require("ui/core/view");
+var dialogs = require("ui/dialogs");
+var buttonModule = require("ui/button");
+const Button = require("tns-core-modules/ui/button/").Button;
+const layout = require("tns-core-modules/ui/layouts/grid-layout");
+var observable = require("data/observable");
+var pageData = new observable.Observable();
+var subNavTitle = "YourPayInformation";
+var applicationSettings = require("application-settings");
+var overTimeList = [];
+var switchModule = require("tns-core-modules/ui/switch");
+var pageObject;
+const Label = require("tns-core-modules/ui/label/").Label;
+
+
+exports.pageLoaded = function(args) {
+    const page = args.object;
+    const TODAY = new Date();
+    pageObject = page;
+    page.bindingContext = pageData;
+    pageData.set("date", TODAY);
+    pageData.set("AddTime", false);
+    getOvertime();
+    displayOvertime();
+};
+exports.navToggle = function(args){
+    subNavTitle = args.object.value;
+    //alert(args.object.value).then(() => {
+    console.log(subNavTitle);
+    //});
+    pageData.set(subNavTitle, !pageData.get(subNavTitle));
+};
+exports.addTime = function(args){
+    var enteredTime = {Date:pageData.get("submitDate"), Hours:pageData.get("submitHours"), Entered:false};
+    addToOvertime(enteredTime);
+}
+exports.goToHome = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("main-page");
+    
+};
+exports.goBack = function(args){
+    const thisPage = args.object.page;
+    thisPage.frame.goBack()
+}
+exports.footer3 = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("profile-page");
+    
+}
+exports.footer4 = function(){
+    console.log("Go To Feedback");
+    var topmost = frameModule.topmost();
+    //topmost.navigate("feedback-page");
+    var pageDetails = String(topmost.currentPage).split("///");
+    const TODAY = new Date();
+    var navigationOptions={
+        moduleName:'feedback-page',
+        context:{Language: "ENG",
+                PageName: pageDetails[1].split("/")[1].split(".")[0],
+                DateTime: TODAY
+                }
+            }
+    topmost.navigate(navigationOptions); 
+}
+exports.footer5 = function(){
+    var topmost = frameModule.topmost();
+    topmost.navigate("POC-page");
+}
+exports.clearSubmittedTime = function(){
+    var tempOvertimeList = [];
+    for(i=0; i<overTimeList.length; i++){
+        if(overTimeList[i].Entered == false){
+            tempOvertimeList.push(overTimeList[i]);
+        }
+    }
+    overTimeList = tempOvertimeList;
+    var saveString = JSON.stringify(overTimeList);
+    applicationSettings.setString("Saved_Overtime", saveString);
+    pageData.set("ViewSchedule", false);
+    saveOvertime();
+    displayOvertime();
+
+}
+var displayOvertime = function(){
+    var otLayout = pageObject.getViewById("Overtime_Stack");
+    otLayout.removeChildren();
+
+    for(i=0; i<overTimeList.length; i++){
+        var gridLayout = new layout.GridLayout();
+        var rowTitle = new Label();
+        var hoursTitle = new Label();
+        var hoursNumber = new Label();
+        var dateInput = new Date(overTimeList[i].Date);
+        var enteredTitle = new Label();
+        var enteredCheck = new Button();
+        var clearButton = new Button();
+        
+        rowTitle.text = `${dateInput.getFullYear()}\/${dateInput.getMonth()+1}\/${dateInput.getDate()}`;
+        rowTitle.className = "Main_Nav_SubLine";
+        hoursTitle.text = "Hours:";
+        hoursTitle.className = "Utility_TableText_1L";
+        hoursNumber.text = overTimeList[i].Hours;
+        hoursNumber.className = "Utility_TableText_1";
+        enteredTitle.text = "Entered?";
+        enteredTitle.className = "Utility_TableText_1L";
+        enteredCheck.className = "Home_Checkbox";
+        enteredCheck.value = overTimeList[i].Entered;
+        if(overTimeList[i].Entered == "true"){enteredCheck.text = "\uea10";};
+        enteredCheck.id = i;
+        enteredCheck.on(buttonModule.Button.tapEvent, setItemEntered, this);
+        clearButton.text = "Clear";
+        clearButton.id = i;
+        clearButton.className = "Submit_Button_1";
+        clearButton.on(buttonModule.Button.tapEvent, clearThisItem, this);
+
+        layout.GridLayout.setRow(rowTitle, 0);
+        layout.GridLayout.setRow(hoursTitle, 1);
+        layout.GridLayout.setRow(hoursNumber, 1);
+        layout.GridLayout.setRow(enteredTitle, 2);
+        layout.GridLayout.setRow(enteredCheck, 2);
+        layout.GridLayout.setRow(clearButton, 3);
+
+        layout.GridLayout.setColumn(rowTitle, 0);
+        layout.GridLayout.setColumn(hoursTitle, 0);
+        layout.GridLayout.setColumn(hoursNumber, 1);
+        layout.GridLayout.setColumn(enteredTitle, 0);
+        layout.GridLayout.setColumn(enteredCheck, 1);
+        layout.GridLayout.setColumn(clearButton, 0);
+        
+        gridLayout.addChild(rowTitle);
+        gridLayout.addChild(hoursTitle);
+        gridLayout.addChild(enteredTitle);
+        gridLayout.addChild(enteredCheck);
+        gridLayout.addChild(hoursNumber);
+        gridLayout.addChild(clearButton);
+
+
+        var titleRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+        var hoursRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+        var checkRow= new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+        var clearRow = new layout.ItemSpec(1, layout.GridUnitType.AUTO);
+        var firstCol = new layout.ItemSpec(1, layout.GridUnitType.STAR);
+        var secondCol = new layout.ItemSpec(1, layout.GridUnitType.STAR);
+        layout.GridLayout.setColumnSpan(rowTitle, 2);
+        layout.GridLayout.setColumnSpan(clearButton, 2);
+        gridLayout.addColumn(firstCol);
+        gridLayout.addColumn(secondCol);
+        gridLayout.addRow(titleRow);
+        gridLayout.addRow(hoursRow);
+        gridLayout.addRow(checkRow);
+        gridLayout.addRow(clearRow);
+        gridLayout.className = "POC_Grid";
+        otLayout.addChild(gridLayout);
+    }
+
+}
+var clearThisItem = function(args){
+    console.log(args.object.id);
+    overTimeList.splice(args.object.id, 1);
+    var saveString = JSON.stringify(overTimeList);
+    applicationSettings.setString("Saved_Overtime", saveString);
+    pageData.set("ViewSchedule", false);
+    saveOvertime();
+    displayOvertime();
+    pageData.set("ViewSchedule", true);
+}
+var setItemEntered = function(args){
+    
+    if(args.object.value == "true"){
+        args.object.value = "false";
+        args.object.text = "";
+        args.object.style.backgroundColor = "#FFF";
+    }else{
+        args.object.value = "true";
+        args.object.text = String.fromCharCode(0xea10);
+        args.object.style.backgroundColor = "#222";
+        
+    }
+    console.log(args.object.id + "| " + args.object.value);
+    overTimeList[args.object.id].Entered = args.object.value;
+    saveOvertime();
+}
+
+var formatDate = function(inputDate){
+    var dateInput = new Date(inputDate);
+    var formattedDate;
+    var month;
+    switch (dateInput.getMonth()){
+    case 0 :
+        month = "January";
+        break;
+    case 1 :
+        month = "February";
+        break;
+    case 2 :
+        month = "March";
+        break;
+    case 3 :
+        month = "April";
+        break;
+    case 4 :
+        month = "May";
+        break;
+    case 5 :
+        month = "June";
+        break;
+    case 6 :
+        month = "July";
+        break;
+    case 7 :
+        month = "August";
+        break;
+    case 8 :
+        month = "September";
+        break;
+    case 9 :
+        month = "October";
+        break;
+    case 10 :
+        month = "November";
+        break;
+    case 11 :
+        month = "December";
+        break;
+    }
+    formattedDate = month + " " + dateInput.getDate() + ", " + dateInput.getFullYear();
+    return formattedDate;
+}
+var getOvertime = function(){
+    var overtimePull;
+    if(applicationSettings.hasKey("Saved_Overtime")){
+        overtimePull = applicationSettings.getString("Saved_Overtime");
+        overTimeList = JSON.parse(overtimePull);
+    }
+    console.log(overTimeList.length);
+}
+var addToOvertime = function(timeToSave)
+{
+    //Get currently saved overtime
+    var overtimePull = null;
+    var saveString = "";
+    if(applicationSettings.hasKey("Saved_Overtime")){
+        overtimePull = applicationSettings.getString("Saved_Overtime");
+    }
+    else {
+    };
+    if(overtimePull == null){
+        var savedOvertime = [];
+        savedOvertime.push(timeToSave);
+        saveString = JSON.stringify(savedOvertime);
+        applicationSettings.setString("Saved_Overtime", saveString);
+    }else{
+        var savedOvertime = JSON.parse(overtimePull);
+        savedOvertime.push(timeToSave);
+        saveString = JSON.stringify(savedOvertime);
+        applicationSettings.setString("Saved_Overtime", saveString);
+    }
+    overTimeList = savedOvertime;
+    pageData.set("AddTime", false);
+    displayOvertime();
+}
+var saveOvertime = function(){
+
+    var saveString = "";
+    saveString = JSON.stringify(overTimeList);
+    applicationSettings.setString("Saved_Overtime", saveString);
+    
+};
