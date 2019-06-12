5845311e49a6994da575c81d44d56ba64cc898d5
Graeme Rombough <graeme_rombough@hotmail.com>
2019-06-11 21:44:37 -0400

    add keyboard manager

--- platforms/ios/build/HRGO.build/Debug-iphonesimulator/HRGO.build/Objects-normal/i386/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/graemerombough/HRGO/platforms/ios/HRGO/HRGO-Prefix.pch \
-  /Users/graemerombough/HRGO/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/graemerombough/HRGO/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
