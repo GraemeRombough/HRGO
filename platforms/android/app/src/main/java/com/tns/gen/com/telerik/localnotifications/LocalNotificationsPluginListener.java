b2f534759a210ed31a6878ec82cc8df4229666fb
Graeme Rombough <graeme_rombough@hotmail.com>
2019-06-12 21:35:18 -0400

    build issues

--- platforms/android/app/src/main/java/com/tns/gen/com/telerik/localnotifications/LocalNotificationsPluginListener.java
+++ platforms/android/app/src/main/java/com/tns/gen/com/telerik/localnotifications/LocalNotificationsPluginListener.java
@@ -5,21 +5,31 @@
  */
 package com.tns.gen.com.telerik.localnotifications;
 
-public class LocalNotificationsPluginListener implements com.telerik.localnotifications.LocalNotificationsPluginListener {
-	public LocalNotificationsPluginListener() {
-		com.tns.Runtime.initInstance(this);
-	}
+public class LocalNotificationsPluginListener extends java.lang.Object
+    implements com.tns.NativeScriptHashCodeProvider,
+        com.telerik.localnotifications.LocalNotificationsPluginListener {
+  public LocalNotificationsPluginListener() {
+    super();
+    com.tns.Runtime.initInstance(this);
+  }
 
-	public void success(java.lang.Object param_0)  {
-		java.lang.Object[] args = new java.lang.Object[1];
-		args[0] = param_0;
-		com.tns.Runtime.callJSMethod(this, "success", void.class, args);
-	}
+  public void success(java.lang.Object param_0) {
+    java.lang.Object[] args = new java.lang.Object[1];
+    args[0] = param_0;
+    com.tns.Runtime.callJSMethod(this, "success", void.class, args);
+  }
 
-	public void error(java.lang.Object param_0)  {
-		java.lang.Object[] args = new java.lang.Object[1];
-		args[0] = param_0;
-		com.tns.Runtime.callJSMethod(this, "error", void.class, args);
-	}
+  public void error(java.lang.Object param_0) {
+    java.lang.Object[] args = new java.lang.Object[1];
+    args[0] = param_0;
+    com.tns.Runtime.callJSMethod(this, "error", void.class, args);
+  }
 
+  public int hashCode__super() {
+    return super.hashCode();
+  }
+
+  public boolean equals__super(java.lang.Object other) {
+    return super.equals(other);
+  }
 }
