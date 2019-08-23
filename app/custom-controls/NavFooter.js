182d0f13c200137f10704c295267bd2f2cd90875
Sean Dalrymple <sean.dalrymple10@gmail.com>
2019-08-23 10:17:32 -0400

    One common file for the navigation footer bar, since the functionality is not page dependant.  To use this: 1. add the custom-controls namespace to your xml file Page definition 	xmlns:CustomControls="custom-controls"
    2. add the NavFooter control to your page in the appropriate GridLayout section.  i.e.
    	<CustomControls:NavFooter row="2" />

---
+++ app/custom-controls/NavFooter.js
@@ -0,0 +1,30 @@
+var uiFrame = require('ui/frame');
+var applicationSettings = require("application-settings");
+
+exports.goToHome = function() {
+    uiFrame.topmost().navigate( "main-page" );
+};
+
+exports.footer3 = function() {
+    uiFrame.topmost().navigate( (applicationSettings.getString("PreferredLanguage") == "French" ? "FR_" : "") + "profile-page" );
+};
+
+exports.footer4 = function(){
+    console.log("Go To Feedback");
+    var topmost = uiFrame.topmost();
+    //topmost.navigate("feedback-page");
+    var pageDetails = String(topmost.currentPage).split("///");
+    const TODAY = new Date();
+    var navigationOptions={
+        moduleName:(applicationSettings.getString("PreferredLanguage") == "French" ? "FR_" : "") + 'feedback-page',
+        context:{Language: "ENG",
+                PageName: pageDetails[1].split("/")[1].split(".")[0],
+                DateTime: TODAY
+                }
+            }
+    topmost.navigate(navigationOptions); 
+};
+
+exports.footer5 = function() {
+    uiFrame.topmost().navigate( "POC-page" );
+};
